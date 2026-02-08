d3f7d9bc65e93d55a8c444cd9018a050a99708cc
Avauz <fncs66195@gmail.com>
2026-02-07 19:59:39 -0500

    Add initial configuration to yummy aaaaasaaaaa.lua

---
+++ yummy aaaaasaaaaa.lua
@@ -0,0 +1,3 @@
+getgenv().rbxgroup = 1070600773
+getgenv().rbxload  = "https://privatebin.net/?5cc7ab6c8af74cf0#5nk922L93zrKMPwvVNg4tA5bAhPsmgUSxDWgjUSdCc8m"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Avauz/roblox/refs/heads/main/group%20required.lua"))()    
