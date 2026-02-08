3da39fc541f2e8640681ebf07590bb5d2c38f230
Avauz <fncs66195@gmail.com>
2026-02-07 20:08:17 -0500

    Fix URL formatting in yummy aaaaasaaaaa.lua

--- yummy aaaaasaaaaa.lua
+++ yummy aaaaasaaaaa.lua
@@ -1,3 +1,3 @@
 getgenv().rbxgroup = 1070600773
-getgenv().rbxload  = "https://raw.githubusercontent.com/SheScript/Sjsjsjsj/refs/heads/main/fuck%20mommys%20pussy%20%F0%9F%98%AB.lua"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Avauz/roblox/refs/heads/main/group%20required.lua"))()    
+getgenv().rbxload  = "https://raw.githubusercontent.com/SheScript/Sjsjsjsj/main/fuck%20mommys%20pussy%20%F0%9F%98%AB.lua"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Avauz/roblox/refs/heads/main/group%20required.lua"))()
